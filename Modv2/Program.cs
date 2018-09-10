92c6d237e51da2873a3863ef1e8a25640211940c
david leon peinado <davidlp1234@hotmail.com>
2018-09-10 15:20:59 -0300

    Este es mi codigo

--- Modv2/Program.cs
+++ Modv2/Program.cs
@@ -4,9 +4,34 @@ namespace Modv2
 {
     class Program
     {
+        static int? Menor(int a, int b)
+        {
+            if (a < b)
+                return a;
+            else if (a == b)
+                return null;
+            else return b;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int primer, segundo
+                ;
+            int? resultado;
+
+            Console.WriteLine("Ingrese el primer numero");
+            primer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            segundo = Convert.ToInt32(Console.ReadLine());
+            resultado = Menor(primer, segundo);
+            if (resultado == null)
+                Console.WriteLine("Ingresaste el mismo numero, son iguales");
+            else
+                Console.WriteLine("{0} es el Menor", resultado);
+            Console.ReadKey();
+
+
         }
     }
 }
